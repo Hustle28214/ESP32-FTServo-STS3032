6977cf0689e2da92c2e916fdad9e978a35d5b025
wavvXD! <2300864263@qq.com>
2025-04-23 14:41:47 +0800

    update STS_ORDER.c

--- STS_ORDER.c
+++ STS_ORDER.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include "STS_ORDER.h"
 
-static uint8_t ORDER = 0; // 0 = LITTLE, 1 = BIG
+static uint8_t Level = 1; // Servo ACK Level
 static uint8_t END = 0; // 0 = LITTLE, 1 = BIG
 static uint8_t servo_status;
 static uint8_t servo_error; 
+
+
 void ORDER_GET_END(uint8_t _END){
     // GET END SETTINGS
     END = _END;
@@ -20,4 +22,34 @@ bool ORDER_IS_BIG_END(uint8_t _END){
     return true;
 }
 
-bool
+bool ORDER_IS_LITTLE_END(uint8_t _END){
+    // RETURN 1 IF LITTLE END, 0 IF BIG END
+    END = _END;
+    if(END == 1){
+        return false;
+    }
+    return true;
+}
+
+void ORDER_GET_LEVEL(void){
+    // GET SERVO ACK LEVEL
+    return Level;
+}
+
+void ORDER_GET_SERVO_STATUS(void){
+    // GET SERVO STATUS
+    return servo_status; 
+}
+
+void ORDER_GET_SERVO_ERROR(void){
+    // GET SERVO ERROR
+    return servo_error;
+}
+
+void ORDER_SET_LEVEL(uint8_t _Level){
+    // SET SERVO ACK LEVEL
+    Level = _Level;
+    return Level; 
+}
+
+void 
